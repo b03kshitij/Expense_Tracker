45e2b15a0c85bc61ea06145cd9f1763fe0141334
Kshitij <kshitijbhosale1111@gmail.com>
2026-03-26 16:49:10 +0530

    final working database config

--- config/database.php
+++ config/database.php
@@ -19,7 +19,5 @@ $conn = new mysqli($host, $user, $pass, $dbname, $port);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 $conn->set_charset("utf8mb4");
-
 ?>
