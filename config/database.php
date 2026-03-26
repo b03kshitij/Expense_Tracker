89cbda97cac25a282a55653904a6ad51d76b27e5
Kshitij <kshitijbhosale1111@gmail.com>
2026-03-26 16:17:17 +0530

    Database Connected

--- config/database.php
+++ config/database.php
@@ -1,11 +1,15 @@
 <?php
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.railway.internal');
 define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'expense_tracker');
+define('DB_PASS', 'wpncgszFbJyUgeTwSuidMOcguufeSnsL');
+define('DB_NAME', 'railway');
+define('DB_PORT', '3306');
 
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 $conn->set_charset("utf8mb4");
+?>
